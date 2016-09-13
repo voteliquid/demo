0a43671ddcf391669651f5303c348cd35c881d15
David Ernst <git@dsernst.com>
2016-09-13 00:07:40 -0700

    Clarify blank vs no_vote, make delegated votes clearer for tallying

--- tally-votes.js
+++ tally-votes.js
@@ -24,33 +24,37 @@ let bill = {
   body: '',
   date_introduced: new Date('Mon Sep 12 2016 04:34:21 GMT-0700 (PDT)'),
   date_of_vote: new Date('Fri Sep 16 2016 17:00:00 GMT-0700 (PDT)'),
-  LD_for: 0,
-  LD_against: 0, // these three values default to 0
-  LD_abstain: 0,
+  votes_yay: 0,// these tally values all default to 0
+  votes_yay_delegated: 0,
+  votes_nay: 0,
+  votes_nay_delegated: 0,
+  votes_blank: 0,
+  votes_blank_delegated: 0,
+  votes_no_vote: 0,
 }
 
 // Generate random votes on exampleItem for all our users
 let votes = []
 for (let i = 0; i < voters.length; i++) {
 
-  // Pick a random position: 'for', 'against', 'abstain' (explicit), 'no vote' (inherits)
-  let position = ['for', 'against', 'abstain', 'no vote'][Math.floor(Math.random() * 4)]
+  // Pick a random position: 'yay', 'nay', 'blank' (explicit abstain), 'no_vote' (inherits)
+  let position = ['yay', 'nay', 'blank', 'no_vote'][Math.floor(Math.random() * 4)]
 
-  if (position !== 'no vote') {
+  if (position !== 'no_vote') {
     votes.push({
       uid: 'vote' + (i + 1),
-      voter: voters[i].uid,
+      voter_uid: voters[i].uid,
       bill: 'exampleItem',
       position,
       date: new Date,
     })
   }
 }
-// console.log('votes:\n', votes)
+console.log('votes:\n', votes, '\n')
 
 // Create indices for quick lookups
 const votersByUid = _.keyBy(voters, 'uid')
-const votesByVoter = _.keyBy(votes, 'voter')
+const votesByVoterUid = _.keyBy(votes, 'voter_uid')
 
 // -------------------------
 // Now the actual vote-tallying alorithm begins
@@ -58,24 +62,24 @@ const votesByVoter = _.keyBy(votes, 'voter')
 
 // Given a voter and the record of all votes,
 // return that individual's voter position (recursive)
-function resolveIndividualsPosition(voter, votesByVoter, cycleState) {
+function resolveIndividualsPosition(voter, votesByVoterUid, cycleState) {
   // Did the voter explicitly vote?
-  if (votesByVoter.hasOwnProperty(voter.uid)) {
-    return votesByVoter[voter.uid].position
+  if (votesByVoterUid.hasOwnProperty(voter.uid)) {
+    return votesByVoterUid[voter.uid].position
   }
 
   // Protect against endless cycle of no-show votes
   cycleState.hare = votersByUid[votersByUid[cycleState.hare.delegate].delegate]
-  if (!votesByVoter.hasOwnProperty(cycleState.hare.uid)) {
+  if (!votesByVoterUid.hasOwnProperty(cycleState.hare.uid)) {
     cycleState.tortoise = votersByUid[cycleState.tortoise.delegate]
     if (cycleState.hare === cycleState.tortoise) {
-      return 'no-vote'
+      return 'no_vote'
     }
   }
 
   // Otherwise inherit their delegate's position
   const delegate = votersByUid[voter.delegate]
-  return resolveIndividualsPosition(delegate, votesByVoter, cycleState)
+  return resolveIndividualsPosition(delegate, votesByVoterUid, cycleState)
 }
 
 
@@ -88,25 +92,23 @@ voters.forEach(voter => {
     hare: voter,
   }
 
-  let position = resolveIndividualsPosition(voter, votesByVoter, cycleState)
-  let isDelegated = !votesByVoter.hasOwnProperty(voter.uid)
+  let position = resolveIndividualsPosition(voter, votesByVoterUid, cycleState)
+  let isDelegated = !votesByVoterUid.hasOwnProperty(voter.uid)
 
-  if (position === 'no-vote') {
+  if (position === 'no_vote') {
     console.log(`${voter.full_name} didn't vote because their delegate chain looped`)
-  } else {
-    console.log(`${voter.full_name} votes "${position}"${isDelegated ? ' (delegated)' : ''}`)
+    bill.votes_no_vote++
+    return
   }
 
-  if (position === 'for') {
-    bill.LD_for++
-  }
+  console.log(`${voter.full_name} votes "${position}"${isDelegated ? ' (delegated)' : ''}`)
 
-  if (position === 'against') {
-    bill.LD_against++
-  }
-
-  if (position === 'abstain' || position === 'no-vote') {
-    bill.LD_abstain++
+  if (position === 'yay') {
+    isDelegated ? bill.votes_yay++ : bill.votes_yay_delegated++
+  } else if (position === 'nay') {
+    isDelegated ? bill.votes_nay++ : bill.votes_nay_delegated++
+  } else if (position === 'blank') {
+    isDelegated ? bill.votes_blank++ : bill.votes_blank_delegated++
   }
 })
 
