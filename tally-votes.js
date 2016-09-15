feed757b72a5a8ad5e171cc63757b3fd758f9c1c
David Ernst <git@dsernst.com>
2016-09-15 13:04:12 -0700

    Lint tally-votes.js

--- tally-votes.js
+++ tally-votes.js
@@ -3,13 +3,13 @@
 
 'use strict'
 
-const _ = require('lodash')
+var _ = require('lodash')
 
 // -------------------------
 // Prep work for example context
 // -------------------------
 
-const voters = require('./example-voters.js') // voters.length === 8
+var voters = require('./example-voters.js') // voters.length === 8
 // console.log(voters[4])
 // // {
 // //   uid: 'e',
@@ -17,14 +17,14 @@ const voters = require('./example-voters.js') // voters.length === 8
 // //   delegate: 'a'
 // // }
 
-let bill = {
+var bill = {
   uid: 'exampleItem',
   name: 'Example Item',
   author: 'e', // voter_uid of 'Eva Ernst'
   body: '',
   date_introduced: new Date('Mon Sep 12 2016 04:34:21 GMT-0700 (PDT)'),
   date_of_vote: new Date('Fri Sep 16 2016 17:00:00 GMT-0700 (PDT)'),
-  votes_yay: 0,// these tally values all default to 0
+  votes_yay: 0, // these tally values all default to 0
   votes_yay_from_delegate: 0,
   votes_nay: 0,
   votes_nay_from_delegate: 0,
@@ -34,34 +34,33 @@ let bill = {
 }
 
 // Generate random votes on exampleItem for all our users
-let votes = []
-for (let i = 0; i < voters.length; i++) {
-
+var votes = []
+for (var i = 0; i < voters.length; i++) {
   // Pick a random position: 'yay', 'nay', 'blank' (explicit abstain), 'no_vote' (inherits)
-  let position = ['yay', 'nay', 'blank', 'no_vote'][Math.floor(Math.random() * 4)]
+  var position = ['yay', 'nay', 'blank', 'no_vote'][Math.floor(Math.random() * 4)]
 
   if (position !== 'no_vote') {
     votes.push({
       uid: 'vote' + (i + 1),
       voter_uid: voters[i].uid,
       bill: 'exampleItem',
-      position,
-      date: new Date,
+      position: position,
+      date: new Date(),
     })
   }
 }
 console.log('votes:\n', votes, '\n')
 
 // Create indices for quick lookups
-const votersByUid = _.keyBy(voters, 'uid')
-const votesByVoterUid = _.keyBy(votes, 'voter_uid')
+var votersByUid = _.keyBy(voters, 'uid')
+var votesByVoterUid = _.keyBy(votes, 'voter_uid')
 
 // -------------------------
 // Now the actual vote-tallying alorithm begins
 // -------------------------
 
 // declare cycleState so resolveIndividualsPosition has access
-let cycleState
+var cycleState
 
 // Given a voter and the record of all votes,
 // return that individual's voter position (recursive)
@@ -81,25 +80,24 @@ function resolveIndividualsPosition(voter, votesByVoterUid) {
   }
 
   // Otherwise inherit their delegate's position
-  const delegate = votersByUid[voter.delegate]
+  var delegate = votersByUid[voter.delegate]
   return resolveIndividualsPosition(delegate, votesByVoterUid)
 }
 
 
 // Tally up the votes by iterating through each voter
-voters.forEach(voter => {
-
+voters.forEach(function (voter) {
   // reset cycleState to implement Floyd's Cycle-Finding Algorithm
   cycleState = {
     tortoise: voter,
     hare: voter,
   }
 
-  let position = resolveIndividualsPosition(voter, votesByVoterUid, cycleState)
-  let isDelegated = !votesByVoterUid.hasOwnProperty(voter.uid)
+  var position = resolveIndividualsPosition(voter, votesByVoterUid, cycleState)
+  var isDelegated = !votesByVoterUid.hasOwnProperty(voter.uid)
 
   // increment tally counter for the appropriate key
-  let tallyKey = 'votes_' + position
+  var tallyKey = 'votes_' + position
   if (position !== 'no_vote' && isDelegated) {
     tallyKey += '_from_delegate'
   }
@@ -110,11 +108,13 @@ voters.forEach(voter => {
 console.log('\nbill:', bill)
 
 // Calculate and print the effective final tallies
-const finalTally = {
+var finalTally = {
   yay: bill.votes_yay + bill.votes_yay_from_delegate,
   nay: bill.votes_nay + bill.votes_nay_from_delegate,
   voted: voters.length - bill.votes_no_vote, // is there a quorum?
   potential_voters: voters.length,
-  delegated: bill.votes_yay_from_delegate + bill.votes_nay_from_delegate + bill.votes_blank_from_delegate,
+  delegated: bill.votes_yay_from_delegate
+    + bill.votes_nay_from_delegate
+    + bill.votes_blank_from_delegate,
 }
 console.log('\nfinalTally:', finalTally)
