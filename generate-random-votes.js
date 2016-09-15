41e6412159a23488119030e965c457f7f4faadcc
David Ernst <git@dsernst.com>
2016-09-15 13:37:24 -0700

    Modularize out generate-random-votes

---
+++ generate-random-votes.js
@@ -0,0 +1,20 @@
+module.exports = function generateRandomVotes(voters) {
+  var votes = []
+  for (var i = 0; i < voters.length; i++) {
+    // Pick a random position: 'yay', 'nay', 'blank' (explicit abstain), 'no_vote' (inherits)
+    var position = ['yay', 'nay', 'blank', 'no_vote'][Math.floor(Math.random() * 4)]
+
+    if (position !== 'no_vote') {
+      votes.push({
+        uid: 'vote' + (i + 1),
+        voter_uid: voters[i].uid,
+        bill: 'exampleItem',
+        position: position,
+        date: new Date(),
+      })
+    }
+  }
+  console.log('votes:\n', votes, '\n')
+
+  return votes
+}
--- tally-votes.js
+++ tally-votes.js
@@ -34,22 +34,7 @@ var bill = {
 }
 
 // Generate random votes on exampleItem for all our users
-var votes = []
-for (var i = 0; i < voters.length; i++) {
-  // Pick a random position: 'yay', 'nay', 'blank' (explicit abstain), 'no_vote' (inherits)
-  var position = ['yay', 'nay', 'blank', 'no_vote'][Math.floor(Math.random() * 4)]
-
-  if (position !== 'no_vote') {
-    votes.push({
-      uid: 'vote' + (i + 1),
-      voter_uid: voters[i].uid,
-      bill: 'exampleItem',
-      position: position,
-      date: new Date(),
-    })
-  }
-}
-console.log('votes:\n', votes, '\n')
+var votes = require('./generate-random-votes.js')(voters)
 
 // Create indices for quick lookups
 var votersByUid = _.keyBy(voters, 'uid')
