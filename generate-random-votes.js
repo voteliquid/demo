269a0e95a9b0247262861f3c58ed6d03c3ba0cf3
David Ernst <git@dsernst.com>
2016-10-28 17:11:01 -0700

    Decrease likelihood that random voter directly votes

--- generate-random-votes.js
+++ generate-random-votes.js
@@ -2,7 +2,7 @@ module.exports = function generateRandomVotes(voters) {
   var votes = []
   for (var i = 0; i < voters.length; i++) {
     // Pick a random position: 'yea', 'nay', 'no_vote' (inherits)
-    var position = ['yea', 'nay', 'no_vote'][Math.floor(Math.random() * 3)]
+    var position = ['yea', 'nay', 'no_vote', 'no_vote'][Math.floor(Math.random() * 4)]
 
     if (position !== 'no_vote') {
       votes.push({
